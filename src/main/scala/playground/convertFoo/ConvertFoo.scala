531e959dc4dec2ac4caedf1cdbeea5bb3cf55b4a
Philipp Gabler <phips.gabler@gmail.com>
2016-05-24 23:30:24 +0200

    Add example for 'undecidable instance'.

--- src/main/scala/playground/convertFoo/ConvertFoo.scala
+++ src/main/scala/playground/convertFoo/ConvertFoo.scala
@@ -25,7 +25,8 @@ class ConvertFoo {
     def convertFoo(a: A): B = convertFooB.convertFoo(convertAFoo.convertFoo(a))
   }
 
-  def evil(i: Int): String = convertFooInstance[Int, String].convertFoo(i)
+//  this doesn't compile:
+//  def evil(i: Int): String = convertFooInstance[Int, String].convertFoo(i)
 
 //    [error] ConvertFoo.scala:28: diverging implicit expansion for type ConvertFoo.this.ConvertFoo[Int,ConvertFoo.this.Foo]
 //    [error] starting with method convertFooInstance in class ConvertFoo
@@ -33,5 +34,29 @@ class ConvertFoo {
 //    [error]                                                ^
 //    [error] one error found
 
+
+
+//  class Bar x
+//  instance (Bar x, Bar y, Eq (x,y)) => Bar (x,y)
+
+  trait Bar[X]
+  trait Eq[X]
+
+  implicit def tupleIsBar[X, Y](implicit barX: Bar[X],
+                                barY: Bar[Y],
+                                eqXY: Eq[(X, Y)]) = new Bar[(X, Y)] {}
+
+  implicit val intIsBar = new Bar[Int] {}
+  implicit val stringIsBar = new Bar[String] {}
+  implicit val intIsEq = new Eq[Int] {}
+  implicit val stringIsEq = new Eq[String] {}
+  implicit def tuplesAreEq[X, Y](implicit eqX: Eq[X], eqY: Eq[Y]) = new Eq[(X, Y)] {}
+
+//  while this does:
+  println(s"witness: ${implicitly[Bar[(Int, String)]]}")
+
+//  scala> new ConvertFoo()
+//  witness: playground.convertFoo.ConvertFoo$$anon$6@5b744434
+//  res0: playground.convertFoo.ConvertFoo = playground.convertFoo.ConvertFoo@18bbea19
 
 }
